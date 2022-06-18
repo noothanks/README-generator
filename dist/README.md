dc7724c110d7b6bb755edb519e152ee90f0c16bd
noothanks <coreyfuller727@yahoo.com>
2022-06-18 11:48:25 -0400

    refactor

---
+++ dist/README.md
@@ -0,0 +1,18 @@
+# test
+  ![License Badge](https://img/shields.io/badge/License-MIT-informational)
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  -[Credits](#credits)
+  - [License](#license)
+
+  ## Installation
+
+  ## Usage
+
+  ##Credits
+
+  ## License
+  
+  This project is licensed under the MIT license
--- index.js
+++ index.js
@@ -1,43 +1,69 @@
 const fs = require('fs');
-const inquirer = require('./inquirer');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions for your project.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide a description of how to use your application.'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Select the license you are using for your project.',
+        choices: ['MIT', 'APACHE2', 'GPL', 'BSD3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Provide instructions on how other developers can contribute.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Explain the testing instructions for your project.'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Provide a link to your GitHub profile.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Provide an email address.'
+    }
+];
+function init() {
+    inquirer
+    .prompt(questions)
+    .then(answers => {
+        const content = generateMarkdown(answers);
 
-const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project?'
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Enter a description for your project'
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'Provide installation instructions for your project.'
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Please provide a description of how to use your application.'
-        },
-        {
-            type: 'checkbox',
-            name: 'license',
-            message: 'Please select the license you are using for your project.',
-            choices: ['MIT', 'ISC']
-        },
-        {
-            type: 'input',
-            name: 'contribute',
-            message: 'Please provide instructions on how other developers can contribute'
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'Please include testing instructions for your project'
-        }
-    ])
+        fs.writeFile('./dist/README.md', content, error => {
+            if (error) {
+                console.log(error);
+            }
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    })
 }
+
+init();
